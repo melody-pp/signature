7a2f370b809a679afa613d56b415c907330fcb9d
皮娟 <trytry8090@163.com>
2018-04-07 23:43:26 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <canvas ref="canvas" height="500" width="500"></canvas>
+    <button @click="save">保存</button>
+  </div>
+</template>
+
+<script>
+  import SignaturePad from 'signature_pad'
+
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        signaturePad: null
+      }
+    },
+    mounted () {
+      this.signaturePad = new SignaturePad(this.$refs.canvas)
+    },
+    methods: {
+      save () {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  canvas {
+    height: 500px;
+    width: 500px;
+    border: 1px solid #000;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="mainBox">
+    <div class="sig-container">
+      <div class="juan-container">
+        <img class="zhou left" src="../assets/juanzhou_bian.png">
+        <img class="juan" src="../assets/juanzhou_zhong.png">
+        <img class="zhou right" src="../assets/juanzhou_bian.png">
+      </div>
+      <canvas ref="canvas" width="655" height="235"></canvas>
+
+      <button @click="setStrokeStyle('thin')">细</button>
+      <button @click="setStrokeStyle('thick')">粗</button>
+      <button @click="clear">清除</button>
+      <button @click="revoke">撤销</button>
+      <button @click="cancelRevoke">取消撤销</button>
+      <button @click="erase">擦除</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SignaturePad from 'signature_pad'
+
+  let signaturePad
+  let revokeStep = 0
+  let revokeStates = []
+  const strokeWidths = {thin: [1, 6], thick: [2, 10]}
+
+  export default {
+    name: 'home',
+    mounted () {
+      signaturePad = new SignaturePad(
+        this.$refs.canvas,
+        {minWidth: 1, maxWidth: 6, onEnd: this.addRevokeState}
+      )
+    },
+    methods: {
+      setStrokeStyle (style) {
+        const [minWidth, maxWidth] = strokeWidths[style]
+
+        signaturePad.minWidth = minWidth
+        signaturePad.maxWidth = maxWidth
+        signaturePad.penColor = '#000'
+      },
+      clear () {
+        signaturePad.clear()
+      },
+      render (dataURL) {
+        signaturePad._reset()
+
+        const img = new Image()
+        const ctx = this.$refs.canvas.getContext('2d')
+
+        img.src = dataURL
+        img.onload = () => ctx.drawImage(img, 0, 0, 655, 235)
+      },
+      addRevokeState () {
+        revokeStates = revokeStates.slice(revokeStep)
+        revokeStates.unshift(signaturePad.toDataURL())
+
+        revokeStep = 0
+      },
+      revoke () {
+        this.clear()
+
+        revokeStep = Math.min(revokeStates.length - 2, revokeStep)
+        this.render(revokeStates[++revokeStep])
+      },
+      cancelRevoke () {
+        this.clear()
+
+        revokeStep = Math.max(1, revokeStep)
+        this.render(revokeStates[--revokeStep])
+      },
+      erase () {
+        signaturePad.minWidth = 10
+        signaturePad.maxWidth = 10
+        signaturePad.penColor = '#fff'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .sig-container {
+    width: 785px;
+    margin: auto;
+    position: relative;
+  }
+
+  .juan-container {
+    font-size: 0;
+  }
+
+  .juan {
+    width: 720px;
+  }
+
+  .zhou {
+    width: 42px;
+    &.left {
+      position: relative;
+      margin-right: -10px;
+    }
+    &.right {
+      margin-left: -10px;
+    }
+  }
+
+  canvas {
+    top: 85px;
+    left: 65px;
+    width: 655px;
+    height: 235px;
+    position: absolute;
+    background: #fff;
+  }
+
+</style>
