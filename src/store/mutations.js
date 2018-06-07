ca1588e5d18418974f78f2914d4ff4af8ae39c69
王跃 <wangyue57@meituan.com>
2018-06-07 17:15:13 +0800

    Revert "no message"
    This reverts commit db56e3e610688147e3449d49d4b5c0be49a6f44e.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,19 +1,19 @@
-const juanWrapper = document.getElementById('juan-wrapper')
-
 export default {
-
   setPageIndex (state, index) {
     state.pageIndex = index
   },
-  toPageIndex0 (state, index) {
-    const {juan} = juanWrapper
+  setIsBusy (state, isBusy) {
+    state.isBusy = isBusy
+  },
+  toPageIndex0 () {
+    this.$store.commit('setPageIndex', 0)
+    const {juan} = this.$refs
     new TimelineLite({
       onComplete: () => {
-        state.isBusy = false
-        state.pageIndex = index
+        setIsBusy(false)
       }
     })
-      .set(this.$refs.juan, {width: 0})
-      .to(this.$refs.juan, 2, {width: '60vw'})
+      .set(juan, {width: 0})
+      .to(juan, 2, {width: '60vw'})
   }
 }
